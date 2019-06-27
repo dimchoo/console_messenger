b282dd681a5c545a36a28986a47920c1dbc331d8
dimchoo <imdimchoo@gmail.com>
2019-06-27 23:45:28 +0300

    Утилиты проекта

---
+++ jim/utils.py
@@ -0,0 +1,49 @@
+import json
+from .config import COMMON_ENCODING
+
+
+def write_bytes(dict_message):
+    """
+    Функция преобразует словарь в JSON-объект
+    и кодирует его в байты
+    :param dict_message: dict (Словарь с данными)
+    :return: bytes (Байтовое представление JSON-объекта)
+    """
+    if isinstance(dict_message, dict):
+        byte_message = json.dumps(dict_message).encode(COMMON_ENCODING)
+        return byte_message
+    raise TypeError
+
+
+def read_bytes(byte_message):
+    """
+    Функция декодирует байты
+    и преобразует их в словарь
+    :param byte_message: bytes (Байтовое представление JSON-объекта)
+    :return: dict (Словарь с данными)
+    """
+    if isinstance(byte_message, bytes):
+        dict_message = json.loads(byte_message.decode(COMMON_ENCODING))
+        if isinstance(dict_message, dict):
+            return dict_message
+        raise TypeError
+    raise TypeError
+
+
+def send_message(sock, message):
+    """
+    Функция отправки сообщения
+    :param sock: socket (Объект сокета)
+    :param message: dict (Словарь сообщения)
+    :return: None
+    """
+    sock.send(write_bytes(message))
+
+
+def get_message(sock):
+    """
+    Функция получения сообщения
+    :param sock: socket (Объект сокета)
+    :return: dict (Словарь сообщения)
+    """
+    return read_bytes(sock.recv(1024))
